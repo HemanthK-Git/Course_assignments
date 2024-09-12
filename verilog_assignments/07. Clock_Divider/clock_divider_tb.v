7740c3ac3525d0d430b194408e467a23676eee57
TEEDA HEMANTH KUMAR <hemanthkumar199925@gmail.com>
2024-09-12 23:12:01 +0530

    Update clock_divider_tb.v

--- verilog_assignments/07. Clock_Divider/clock_divider_tb.v
+++ verilog_assignments/07. Clock_Divider/clock_divider_tb.v
@@ -11,13 +11,13 @@ clock_divider dut(.clk(clk),.rst(rst),.dum_clk(dum_clk));
 initial
     begin
     clk=0;
-    forever #5 clk=~clk;
+    forever #3 clk=~clk;
     end
     
 initial 
     begin
     rst=1;
-    #10 rst=0;
+    #5 rst=0;
     end
 
 endmodule
